c70df61ca1b99f2363291c5d48ac9743473637b9
Madr50 <178860924+Madr50@users.noreply.github.com>
2026-07-21 00:32:13 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8715509091:AAEaIV-tEfz6KYWwG9vOpTEQ87LrJbxbw_0")
 ADMIN_ID = 8989271393
 ADMIN_USERNAME = "l825h"
 SHOP_NAME = "Twitter X Shop"
